5e5688713b0ff9565648232d2e8fb34a258d7e2b
Rijoo <rijoo1121@gmail.com>
2023-02-16 13:11:21 +0900

    Add: 데이터 저장 및 삭제 기능 추가, const 파일 추가
    sharedpreferences로 데이터 저장, 삭제 버튼 및 전화 버튼 추가, 실행기능 추가

--- emerg_medical_info/app/src/main/java/fastcampus/part1/emerg_medical_info/EditActivity.kt
+++ emerg_medical_info/app/src/main/java/fastcampus/part1/emerg_medical_info/EditActivity.kt
@@ -1,9 +1,14 @@
 package fastcampus.part1.emerg_medical_info
 
+import android.app.DatePickerDialog
+import android.app.DatePickerDialog.OnDateSetListener
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.ArrayAdapter
+import android.widget.Toast
+import androidx.core.view.isVisible
 import fastcampus.part1.emerg_medical_info.databinding.ActivityEditBinding
 import fastcampus.part1.emerg_medical_info.databinding.ActivityMainBinding
 
@@ -21,5 +26,54 @@ class EditActivity : AppCompatActivity() {
             R.array.blood_types,
             android.R.layout.simple_list_item_1
         )
+
+        binding.birthdateLayer.setOnClickListener {
+            val listener = OnDateSetListener{ _, year, month, dayOfMonth ->
+                binding.birthdateTextView.text = "$year-${month.inc()}-$dayOfMonth"
+            }
+            DatePickerDialog(
+                this,
+                listener,
+                2000,
+                1,
+                1
+            ).show()
+        }
+        
+        binding.warningCheckBox.setOnCheckedChangeListener { _, isChecked ->
+            binding.warningEditText.isVisible = isChecked
+        }
+
+        binding.warningEditText.isVisible = binding.warningCheckBox.isChecked
+
+        binding.saveButton.setOnClickListener {
+            saveData()
+            finish() //입력 끝나고 MainActivity로 돌아가기 위함
+        }
+    }
+
+    //데이터 저장 함수
+    private fun saveData(){
+        with(getSharedPreferences(USER_INFORMATION, Context.MODE_PRIVATE).edit()) {
+            putString(NAME, binding.nameEditText.text.toString())
+            putString(BLOOD_TYPE, getBloodType())
+            putString(EMERGENCY_CONTACT, binding.phoneEditText.text.toString())
+            putString(BIRTHDATE, binding.birthdateTextView.text.toString())
+            putString(WARNING, getWarning())
+            apply()
+        }
+        Toast.makeText(this, "저장을 완료했습니다.", Toast.LENGTH_SHORT).show()
+    }
+
+    //혈액형 get 함수
+    private fun getBloodType() : String {
+        val bloodAlphabet = binding.bloodTypeSpinner.selectedItem.toString()
+        var bloodSign = if(binding.bloodTypePlus.isChecked) "+" else "-"
+        return "$bloodSign$bloodAlphabet"
+    }
+
+    //주의사항 get 함수
+    private fun getWarning() :String {
+        return if(binding.warningCheckBox.isChecked) binding.warningEditText.text.toString() else ""
     }
 }
--- emerg_medical_info/app/src/main/java/fastcampus/part1/emerg_medical_info/MainActivity.kt
+++ emerg_medical_info/app/src/main/java/fastcampus/part1/emerg_medical_info/MainActivity.kt
@@ -1,8 +1,12 @@
 package fastcampus.part1.emerg_medical_info
 
+import android.content.Context
 import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.core.view.isVisible
 import fastcampus.part1.emerg_medical_info.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -14,8 +18,54 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         binding.goInputActivityButton.setOnClickListener {
+            //명시적 인텐트
             val intent = Intent(this, EditActivity::class.java)
             startActivity(intent)
         }
+
+        binding.deleteButton.setOnClickListener {
+            deleteData()
+        }
+
+        binding.emergencyContactLayer.setOnClickListener {
+            //암시적 인텐트
+            with(Intent(Intent.ACTION_VIEW)) {
+                val phoneNumber = binding.phoneValueTextView.text.toString()
+                    .replace("-", "")
+                data = Uri.parse("tel:$phoneNumber")
+                startActivity(this)
+            }
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        getDateUiUpdate()
+    }
+
+    private fun getDateUiUpdate() {
+        with(getSharedPreferences(USER_INFORMATION, Context.MODE_PRIVATE)) {
+            binding.nameValueTextView.text = getString(NAME, "미정")
+            binding.birthdateValueTextView.text = getString(BIRTHDATE, "미정")
+            binding.bloodValueTextView.text = getString(BLOOD_TYPE, "미정")
+            binding.phoneValueTextView.text = getString(EMERGENCY_CONTACT, "미정")
+            val warning = getString(WARNING, "")
+
+            binding.warningTextView.isVisible = warning.isNullOrEmpty().not()
+            binding.warningValueTextView.isVisible = warning.isNullOrEmpty().not()
+            if(!warning.isNullOrEmpty()){
+                binding.warningValueTextView.text = warning
+            }
+            binding.warningValueTextView.text = getString(WARNING, "미정")
+        }
+    }
+
+    private fun deleteData() {
+        with(getSharedPreferences(USER_INFORMATION, MODE_PRIVATE).edit()){
+            clear()
+            apply()
+            getDateUiUpdate()
+        }
+        Toast.makeText( this, "초기화를 완료했습니다.", Toast.LENGTH_SHORT).show()
     }
 }
