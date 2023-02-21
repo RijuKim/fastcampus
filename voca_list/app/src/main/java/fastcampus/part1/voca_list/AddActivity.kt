be8747b3d866cc19b9d87126f8cb065e9fa2a771
Rijoo <rijoo1121@gmail.com>
2023-02-21 15:21:03 +0900

    Create: voca_list UI 구성 및 recyclerview, chip 기능 구현

---
+++ voca_list/app/src/main/java/fastcampus/part1/voca_list/AddActivity.kt
@@ -0,0 +1,37 @@
+package fastcampus.part1.voca_list
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.google.android.material.chip.Chip
+import fastcampus.part1.voca_list.databinding.ActivityAddBinding
+import fastcampus.part1.voca_list.databinding.ItemWordBinding
+
+class AddActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityAddBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityAddBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        initViews()
+    }
+
+    private fun initViews() {
+        val types = listOf(
+            "명사", "동사", "대명사", "형용사", "부사", "감탄사", "전치사", "접속사"
+        )
+        binding.typeChipGroup.apply {
+            types.forEach{ text ->
+                addView(createChip(text))
+            }
+        }
+    }
+
+    private fun createChip(text: String) : Chip {
+        return Chip(this).apply {
+            setText(text)
+            isCheckable = true
+            isClickable = true
+        }
+    }
+}
---
+++ voca_list/app/src/main/java/fastcampus/part1/voca_list/Word.kt
@@ -0,0 +1,7 @@
+package fastcampus.part1.voca_list
+
+data class Word(
+    val text : String,
+    val mean : String,
+    val type : String,
+)
